7c672ddac42759ace0dac4b1f7047d02907d485a
Yago Gomes Tomé de Sousa <yago.tome@hotmail.com>
2016-05-07 11:15:15 -0300

    Criar seletor de arquivos para upload (incompleto).

---
+++ WebService/ClickTrauma/WebContent/WEB-INF/views/pergunta/fileUpload.jsp
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<body>
+
+	<div>
+		<form method="POST" enctype="multipart/form-data" action="insereFile">
+			<table>
+				<tr><td>File to upload:</td><td><input type="file" name="file"></td></tr>
+				<tr><td>Name:</td><td><input name="name"></td></tr>
+				<tr><td></td><td><button>Upload</button></td></tr>
+			</table>
+		</form>
+	</div>
+
+</body>
+</html>
--- WebService/ClickTrauma/src/main/java/br/com/yagotome/clicktrauma/controller/PerguntaController.java
+++ WebService/ClickTrauma/src/main/java/br/com/yagotome/clicktrauma/controller/PerguntaController.java
@@ -1,5 +1,8 @@
 package br.com.yagotome.clicktrauma.controller;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -9,7 +12,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.yagotome.clicktrauma.dao.PerguntaDao;
 import br.com.yagotome.clicktrauma.dao.PerguntaDaoImpl;
@@ -52,5 +60,43 @@ public class PerguntaController {
 		p.setId(id);
 		dao.remove(p);
 		response.setStatus(200);
+	}
+	@RequestMapping(method = RequestMethod.GET, value = "/pergunta/insereFile")
+	public String abreFileUpload() {
+		return "pergunta/fileUpload";
+	}
+	@RequestMapping(method = RequestMethod.POST, value = "/pergunta/insereFile")
+	public String handleFileUpload(@RequestParam("name") String name,
+								   @RequestParam("file") MultipartFile file/*,
+								   RedirectAttributes redirectAttributes*/) {		
+		if (name.contains("/")) {
+			//redirectAttributes.addFlashAttribute("message", "Folder separators not allowed");
+			return "redirect:/pergunta/insereFile";
+		}
+		if (name.contains("/")) {
+			//redirectAttributes.addFlashAttribute("message", "Relative pathnames not allowed");
+			return "redirect:/pergunta/insereFile";
+		}
+		
+		if (!file.isEmpty()) {
+			try {
+				BufferedOutputStream stream = new BufferedOutputStream(
+						new FileOutputStream(new File(name)));
+                FileCopyUtils.copy(file.getInputStream(), stream);
+				stream.close();
+				//redirectAttributes.addFlashAttribute("message",
+					//	"You successfully uploaded " + name + "!");
+			}
+			catch (Exception e) {
+				//redirectAttributes.addFlashAttribute("message",
+					//	"You failed to upload " + name + " => " + e.getMessage());
+			}
+		}
+		/*else {
+			redirectAttributes.addFlashAttribute("message",
+					"You failed to upload " + name + " because the file was empty");
+		}*/
+		
+		return "redirect:/pergunta/insereFile";
 	}
 }
