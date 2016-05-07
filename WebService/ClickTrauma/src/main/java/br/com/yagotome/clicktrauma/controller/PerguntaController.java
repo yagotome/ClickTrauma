044f32ecd17c2876edf934d8fa8a0abf909138c9
Yago Gomes Tomé de Sousa <yago.tome@hotmail.com>
2016-05-07 17:02:27 -0300

    Criar opção para enviar foto para pergunta e respostas no cadastro de perguntas.

--- WebService/ClickTrauma/src/main/java/br/com/yagotome/clicktrauma/controller/PerguntaController.java
+++ WebService/ClickTrauma/src/main/java/br/com/yagotome/clicktrauma/controller/PerguntaController.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URLDecoder;
 import java.util.List;
+import java.util.Stack;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -41,17 +42,74 @@ public class PerguntaController {
 	public String cadastraPergunta() {
 		return "pergunta/cadastro";
 	}
-	@RequestMapping("/pergunta/insere")
-	public String inserePerguta(Pergunta pergunta, Model model) {
-		if (pergunta.getRespostas().get(4).getTexto().isEmpty())
+	@RequestMapping(value = "/pergunta/insere", method = RequestMethod.POST)
+	public String inserePerguta(Pergunta pergunta,
+								@RequestParam("img-pergunta") MultipartFile imgPergunta,
+								@RequestParam("img-resp-0") MultipartFile imgResp0,
+								@RequestParam("img-resp-1") MultipartFile imgResp1,
+								@RequestParam("img-resp-2") MultipartFile imgResp2,
+								@RequestParam("img-resp-3") MultipartFile imgResp3,
+								@RequestParam("img-resp-4") MultipartFile imgResp4,
+								Model model,
+								ServletRequest request) {
+		int qtdResp = 5;
+		if (pergunta.getRespostas().get(4).getTexto().isEmpty()) {
 			pergunta.getRespostas().remove(4);
+			qtdResp = 4;
+		}
 		for (Resposta resposta : pergunta.getRespostas()) {
-			resposta.setPergunta(pergunta);			
+			resposta.setPergunta(pergunta);
 		}
 		dao.insere(pergunta);
+		try {
+			String path = request.getServletContext().getRealPath("/") 
+					+ request.getServletContext().getInitParameter("ImagesPath");		
+			
+			if (imgPergunta != null && !imgPergunta.isEmpty()) {
+				String extension = extensao(imgPergunta.getOriginalFilename());
+				String nome = "perguntas/" + pergunta.getId() + extension;
+				salvaArq(imgPergunta, path + nome);
+			}
+			
+			MultipartFile[] resps = new MultipartFile[]{imgResp0, imgResp1, imgResp2, imgResp3, imgResp4};
+			for (int i = 0; i < qtdResp; i++) {
+				if (resps[i] != null && !resps[i].isEmpty()) {
+					String extension = extensao(resps[i].getOriginalFilename());
+					String nome = "respostas/" + pergunta.getRespostas().get(i).getId() + extension;
+					salvaArq(resps[i], path + nome);
+				}
+			}
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 		model.addAttribute("inserido", true);
 		return "redirect:cadastro";
 	}
+	
+	private String extensao(String nomeArq) {
+		String aux = nomeArq;
+		System.out.println(aux);
+		String revExtension = "";
+		for (int i = aux.length()-1; i >= 0 && aux.charAt(i) != '.'; i--) {
+			revExtension += aux.charAt(i);
+		}
+		String extension = "";
+		for (int i = revExtension.length()-1; i >= 0; i--) {
+			extension += revExtension.charAt(i);
+		}
+		return "." + extension;
+	}
+	
+	private void salvaArq(MultipartFile arq, String nome) throws Exception {
+		File arquivo = new File(nome);
+		System.out.println(arquivo.getAbsolutePath());
+		BufferedOutputStream stream = new BufferedOutputStream(
+				new FileOutputStream(arquivo));
+        FileCopyUtils.copy(arq.getInputStream(), stream);
+		stream.close();	
+	}
+	
 	@RequestMapping("/pergunta/lista")
 	public String lista(Model model) {
 		List<Pergunta> perguntas = dao.lista();
