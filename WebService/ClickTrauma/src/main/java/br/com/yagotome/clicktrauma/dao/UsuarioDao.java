8178da70802c7439eace0e41aaa3bb7316248a96
Yago Gomes Tomé de Sousa <yago.tome@hotmail.com>
2016-05-29 19:15:26 -0300

    Excluir WebService para recriação

--- WebService/ClickTrauma/src/main/java/br/com/yagotome/clicktrauma/dao/UsuarioDao.java
+++
@@ -1,9 +0,0 @@
-package br.com.yagotome.clicktrauma.dao;
-
-import br.com.yagotome.clicktrauma.modelo.Usuario;
-
-public interface UsuarioDao {
-	void insere(Usuario usuario);
-	void atualiza(Usuario usuario);
-	Usuario buscaPorId(Long id);
-}
