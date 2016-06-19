98d33d652e3a43d97a7ebc457b4cce23c1d3f334
Yago Gomes Tomé de Sousa <yago.tome@hotmail.com>
2016-06-19 14:49:50 -0300

    Mudar templates

---
+++ App/www/js/controllers.js
@@ -0,0 +1,38 @@
+angular.module('app.controllers', [])
+  
+.controller('cameraTabDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('cartTabDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('cloudTabDefaultPageCtrl', function($scope) {
+
+})
+      
+.controller('cadastroCtrl', function($scope) {
+
+})
+   
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('menuCtrl', function($scope) {
+
+})
+   
+.controller('comoDiagnosticarCtrl', function($scope) {
+
+})
+   
+.controller('quizCtrl', function($scope) {
+
+})
+   
+.controller('respostaCtrl', function($scope) {
+
+})
+ 
